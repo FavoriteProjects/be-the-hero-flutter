14290b9525666bec24cce80101271d027fe414b3
Robson <robsonsilv410@gmail.com>
2020-03-26 23:26:36 -0300

    :tada: initial project

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:be_the_hero/ui/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Be The Hero',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
